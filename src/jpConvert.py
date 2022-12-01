508ae7ac431e0f0b6dd35ecfeb9ed7104b7ad76c
snomiao <snomiao@gmail.com>
2022-12-01 12:06:23 +0900

    feat(rime-snomiao): wubi kanji

--- src/jpConvert.py
+++ src/jpConvert.py
@@ -6,13 +6,17 @@
 
 from pathlib import Path
 import opencc
-converter = opencc.OpenCC('t2jp.json')
-i = 'Rime/pinyin_japanese.dict.yaml'
-o = 'Rime/pinyin_japanese.dict.yaml'
+s2t = opencc.OpenCC('s2t.json')
+t2jp = opencc.OpenCC('t2jp.json')
+# i = 'Rime/pinyin_japanese.dict.yaml'
+# o = 'Rime/pinyin_japanese.dict.yaml'
+i = 'Rime/wubi86.dict.yaml'
+o = 'Rime/wubi86_japanese.dict.yaml'
 s = ''
 with open(i) as f:
     s = f.read()
-s = converter.convert(s)
+s = s2t.convert(s)
+s = t2jp.convert(s)
 with open(o, 'w') as f:
     f.write(s)
 
