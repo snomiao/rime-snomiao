181452785b369fc92e73cf910cbfb2e20595c151
snomiao <snomiao@gmail.com>
2023-01-07 20:04:48 +0900

    fix(tools): deprecated robocopy

--- tools/deploy.bat
+++ tools/deploy.bat
@@ -3,12 +3,13 @@ cd %~dp0
 cd ..
 
 
-@REM clean logs
+@REM clean outdated logs
 del /S /Q logs\*
 del /S /Q %TEMP%\rime.weasel.*
 
+@REM depreacted, use junction link to develop
 @REM copy files
-robocopy Rime %APPDATA%\Rime *.yaml *.json *.txt *.ocd *.ocd2 /S
+@REM robocopy Rime %APPDATA%\Rime *.yaml *.json *.txt *.ocd *.ocd2 /S
 
 @REM deploy
 cd C:\Program Files (x86)\Rime\weasel-*
