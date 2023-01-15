a51febf4b9f6b441730cbd520f7cc165421c47a0
snomiao <snomiao@gmail.com>
2023-01-15 17:48:58 +0900

    fix(rime-snomiao): sno-english

--- src/zhen-cc-ce/index.ts
+++ src/zhen-cc-ce/index.ts
@@ -3,11 +3,11 @@ import esMain from "es-main";
 import { writeFile } from "fs/promises";
 import { globby } from "globby";
 import { flatMap } from "lodash-es";
-import { filter, map, pipe, sortBy } from "rambda";
+import { filter, map, pipe, sortBy, uniq } from "rambda";
 import snohmr from "snohmr";
 import workPackageDir from "work-package-dir";
 import MDictTypescript from "../lib/mdictTypescript";
-
+import { format } from "prettier";
 if (esMain(import.meta)) await index();
 
 export default async function index() {
@@ -36,35 +36,46 @@ async function dictLoad() {
 
 export async function parse(pairs: string[][]) {
   console.log(chalk.bgWhite("====================================="));
-  let k = 1000;
+  let k = 10;
   const zhjpWordCC = pipe(
     () => pairs,
     sortBy(Math.random),
-    (e) => e.slice(0, 100),
+    // (e) => e.slice(0, 100),
     filter(([k, v]) => Boolean(v)),
     filter(([word, v]) => !word.match(/[\s]/)),
     map(([word, definition]) => {
       const text = definitionTextWash(definition);
       return { word, text, definition };
     }),
-    sortBy(({ text }) => -text.length),
+    sortBy(({ text }) => text.length),
     (e) =>
       flatMap(e, ({ word, text, definition }) => {
-        const enWords = wordsMatch(text, word);
-        if (!enWords.length) return [];
-        if (text && k--) {
+        const enPharses = wordsMatch(text, word);
+        if (!enPharses.length) return [];
+        const zhWord = word.replace(/，/g, "");
+        const enWords = uniq(enPharses)
+          .map((phrase) => phrase.replace(/^to /g, ""))
+          .map((e) => e.trim())
+          .map((e) => e.replace(/ /g, "\u00A0"));
+        if ((text && k) || word === "和") {
+          k && k--;
           console.log(chalk.bgBlue(word));
-          console.log(chalk.blue(definition));
+          console.log(chalk.blue(format(definition, { parser: "html" })));
           console.log(chalk.red(text));
           console.log(
-            chalk.green(enWords.join("\n").replace(/^.+/gm, (e) => "> " + e))
+            chalk.green(enWords.join("\n").replace(/^.+/gm, (e) => `> ${e}.`))
           );
           console.log("");
         }
-        return [`${word}	${[word, ...enWords].join(" ")}`];
+        return [
+          `${zhWord}	${[
+            // word,
+            ...enWords.map((w) => `${w}\u00A0`),
+          ].join(" ")}`,
+        ];
       }),
     // sortBy((e) => -e.length),
-    (e) => e.join("\r\n")
+    (e) => e.join("\n")
   )();
   await writeFile("Rime/opencc/zhen_word.txt", zhjpWordCC);
   // console.log(sample.slice(0, 1000));
@@ -74,7 +85,6 @@ function wordsMatch(text: string, word: string) {
   const words = (text.match(/.+/g) || [])
     .map((e) => e.trim())
     .filter(Boolean)
-
     .map((e) => e.trim())
     .filter(Boolean);
   return words;
@@ -85,17 +95,23 @@ function definitionTextWash(definition: string) {
     definition
       // chars
       .replace(/[\x00\r]/gm, "")
-      .replace(/[,;.-]/gm, "")
-      //
-      .replace(/^@@@LINK=.*/gm, "")
-      // .replace(/(?<=<\/.*?>)/gm, "\n")
+      // quotes
+      // link
+      .replace(/^@@@LINK=.*(?:\n.*)*/gm, "")
       // mark
       .replace(/<dt>(.*?)<\/dt>/gm, (_, $1) => $1 + "\n")
-      .replace(/<div class="py">.*?<\/div>/gm, "")
+      .replace(/<div class="py2?">.*?<\/div>/gm, "")
+      .replace(/<a href="entry:\/\/(.*?)">.*?<\/a>/gm, (_, $1) => `〔${$1}〕`)
+      .replace(/<span class="hpy">.*?<\/span>/gm, "")
       // quotes
       .replace(/<.*?>/gm, "") // html tags
-      .replace(/\(.*?\)/gm, "") // html tags
+      .replace(/\(.*?\)/gm, "")
       .replace(/【.*】/gm, "")
+      .replace(/^(?:lit|fig)\./gm, "")
+      .replace(/^Taiwan pr\..*/gm, "")
+      .replace(/[;.-]/gm, "")
+      .replace(/ 〔/gm, "〔")
+      .replace(/  +/gm, " ")
       .trim()
   );
 }
