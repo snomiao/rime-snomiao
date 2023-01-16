3f64d0d50af5f2bc884aff00e79df6bbe21f1e19
snomiao <snomiao@gmail.com>
2023-01-17 00:07:29 +0900

    fix(mdxFileParse): def

--- src/lib/mdxFileParse/index.ts
+++ src/lib/mdxFileParse/index.ts
@@ -3,6 +3,9 @@ import MDictTypescript from "../mdictTypescript";
 export default async function mdxFileParse(mdxFile: string) {
   const mdx = new MDictTypescript(mdxFile);
   const words = mdx.keyList.map(({ keyText }) => keyText);
-  const entires = words.map((word) => [word, mdx.lookup(word).definition]);
+  const entires = words.map(
+    (word) =>
+      [word, mdx.lookup(word).definition?.replace(/[\x00\r]/gm, "")] as const
+  );
   return entires;
 }
