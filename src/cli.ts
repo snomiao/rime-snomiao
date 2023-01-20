d4005fba2bb2be0ebccde26fef885ec156779db5
snomiao <snomiao@gmail.com>
2023-01-20 21:56:49 +0900

    fix(rime-snomiao): cli-node head

--- src/cli.ts
+++ src/cli.ts
@@ -1,21 +1,25 @@
-import snorun from "snorun";
+#!/usr/bin/env node
 import aswitcher from "aswitcher";
-import { pick, prop } from "rambda";
+import snorun from "snorun";
 // TODO
-await aswitcher(process.platform, {
-  aix: async () => console.log("auto install not supported"),
-  android: async () => console.log("auto install not supported"),
-  cygwin: async () => console.log("auto install not supported"),
-  darwin: async () => {
-    await snorun("cp -r rime-snomiao/Rime/* ~/Library/Rime");
-    console.log("go to your task bar, find <deploy> button and click.");
-  },
-  freebsd: async () => console.log("auto install not supported"),
-  haiku: async () => console.log("auto install not supported"),
-  linux: async () => console.log("auto install not supported"),
-  netbsd: async () => console.log("auto install not supported"),
-  openbsd: async () => console.log("auto install not supported"),
-  sunos: async () => console.log("auto install not supported"),
-  win32: async () => await snorun("cd tools && install"),
-  //() => console.log("not supported"),
-});
+{
+  main();
+}
+async function main() {
+  await aswitcher(process.platform, {
+    aix: async () => console.log("auto install not supported"),
+    android: async () => console.log("auto install not supported"),
+    cygwin: async () => console.log("auto install not supported"),
+    darwin: async () => {
+      await snorun("cp -r rime-snomiao/Rime/* ~/Library/Rime");
+      console.log("go to your task bar, find <deploy> button and click.");
+    },
+    freebsd: async () => console.log("auto install not supported"),
+    haiku: async () => console.log("auto install not supported"),
+    linux: async () => console.log("auto install not supported"),
+    netbsd: async () => console.log("auto install not supported"),
+    openbsd: async () => console.log("auto install not supported"),
+    sunos: async () => console.log("auto install not supported"),
+    win32: async () => await snorun("cd tools && install"),
+  });
+}
