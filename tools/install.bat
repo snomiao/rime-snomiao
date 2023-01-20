62371a936d1be182935a4917fb08e333766619a1
snomiao <snomiao@gmail.com>
2023-01-20 13:29:41 +0900

    fix(tools): deploy in install bat

--- tools/install.bat
+++ tools/install.bat
@@ -1,5 +1,10 @@
 cd %~dp0\..
 robocopy Rime %APPDATA%\Rime *.yaml *.json *.txt *.ocd *.ocd2 /S
+
+echo weasel deploy start
+cd C:\Program Files (x86)\Rime\weasel-*
+
 WeaselDeployer.exe
 start "" WeaselServer.exe
+
 cd %~dp0\..
