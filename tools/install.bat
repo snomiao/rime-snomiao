ae44cd0344a7bbc62f6514987f7f15cf53a77796
snomiao <snomiao@gmail.com>
2023-10-26 00:40:39 +0900

    fix(rime-snomiao): beta
    beta

--- tools/install.bat
+++ tools/install.bat
@@ -1,7 +1,8 @@
 cd %~dp0\..
 
 echo [RIME-SNOMIAO] receipe installing...
-robocopy ./ %APPDATA%\Rime *.yaml /S
+
+robocopy ./ %APPDATA%\Rime *.yaml /S /XD node_modules /XF pnpm-lock.yaml
 robocopy ./opencc %APPDATA%\Rime\opencc *.json *.txt *.ocd *.ocd2 /S
 
 echo [RIME-SNOMIAO] Weasel Deploy
