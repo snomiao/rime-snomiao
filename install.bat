9aed49eb0510caa94600f7edaf6b2a779ff176d6
snomiao <snomiao@gmail.com>
2024-05-17 14:07:11 +0900

    fix(rime-snomiao): crlf and encodings
    crlf and encodings

--- install.bat
+++ install.bat
@@ -1,23 +1,25 @@
+﻿@chcp 65001
 @echo off
-chcp 65001
 
+@REM cmd /U /c install.bat
 cd %~dp0\
 
-echo [RIME-SNOMIAO] 接下準備啓動輸入法安装程序
+echo "[RIME-SNOMIAO] 接下準備啓動輸入法安装程序"
 pause
 
-echo [RIME-SNOMIAO] 正在啓動 weasel 輸入法安装程序，請手動完成安装，然後回到这里
-start "" weasel*.exe
+echo "[RIME-SNOMIAO] 正在啓動 weasel 輸入法安装程序，請手動完成安装，然後回到这里"
+start "" weasel-0.15.0.0-installer.exe
 
+
+echo "[RIME-SNOMIAO] 接下来準備安装輸入方案"
 pause
 
-echo [RIME-SNOMIAO] receipe installing...
-echo [RIME-SNOMIAO] 正在安装輸入方案
+echo "[RIME-SNOMIAO] 正在安装輸入方案"
 
 robocopy ./Rime %APPDATA%\Rime *.yaml /S /XD node_modules /XF pnpm-lock.yaml
 robocopy ./Rime/opencc %APPDATA%\Rime\opencc *.json *.txt *.ocd *.ocd2 /S
 
-echo [RIME-SNOMIAO] Weasel Deploy
+echo "[RIME-SNOMIAO] Weasel Deploy"
 cd C:\Program Files (x86)\Rime\weasel-*
 taskkill /f /im WeaselServer.exe
 
